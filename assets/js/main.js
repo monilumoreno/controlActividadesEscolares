9c074077bf364caddf4d1ccade45b169c6e20584
monilumoreno <monilumoreno@gmail.com>
2021-12-01 09:44:21 -0500

    integracion formularios

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,6 @@
+/**
+ * inicio bloque ocultar y mostrar formularios perfil administrador
+ */
 
 //funcion para mostrar el formulario docentes
 function mostrarDocentes(){
@@ -29,6 +32,35 @@ function ocultarAcudientes(){
     document.getElementById('acudientes').style.display = 'none';
 }
 
+/**
+ * Fin bloque ocultar y mostrar formularios perfil administrador
+ */
+
+
+
+/**
+ * inicio bloque ocultar y mostrar formularios perfil docente
+ */
+
+//funcion para mostrar el formulario registrar actividades
+function mostrarActividades(){
+    document.getElementById('actividades').style.display = 'block';
+}
+
+//funcion para ocultar el formulario docentes
+function ocultarActividades(){
+    document.getElementById('actividades').style.display = 'none';
+}
+/**
+ * Fin bloque ocultar y mostrar formularios perfil docente
+ */
+
+
+
+/**
+ * Inicio bloque validación formularios perfil administrador
+ */
+
 (function() {
     'use strict';
     window.addEventListener('load', function() {
@@ -48,7 +80,6 @@ function ocultarAcudientes(){
   })();
 
   
-
   function verificarPasswords() {
 
   password1 = document.getElementById('ingresoPassword');
@@ -84,3 +115,198 @@ function ocultarAcudientes(){
   }
 
   }
+
+/**
+ * fin bloque validación formularios perfil administrador
+ */
+
+
+/**
+ * Inicio bloque validación grados y materias perfil acudiente
+ */
+
+//función para mostrar materias:
+
+function grados() {
+    grado = document.getElementById('grado');
+
+    var prueba = grado.value;
+    limpiar(); //funcion invocada para limpiar dropdown de materias
+    switch (prueba) {
+        case "6-1":
+
+            var materias = ["Biología",
+                "Agropecuaria",
+                "Español",
+                "Informatica",
+                "Tecnología",
+                "Artística",
+                "Educación Física",
+                "Ética",
+                "Matemáticas",
+                "Inglés",
+                "Sociales"
+            ];
+            break;
+        case "6-2":
+
+            var materias = ["Biología",
+                "Agropecuaria",
+                "Español",
+                "Informatica",
+                "Tecnología",
+                "Artística",
+                "Educación Física",
+                "Ética",
+                "Matemáticas",
+                "Inglés",
+                "Sociales"
+            ];
+
+            break;
+        case "7-1":
+            var materias = ["Biología",
+                "Agropecuaria",
+                "Español",
+                "Informatica",
+                "Tecnología",
+                "Artística",
+                "Educación Física",
+                "Ética",
+                "Matemáticas",
+                "Inglés",
+                "Sociales"
+            ];
+
+            break;
+        case "7-2":
+            var materias = ["Biología",
+                "Agropecuaria",
+                "Español",
+                "Informatica",
+                "Tecnología",
+                "Artística",
+                "Educación Física",
+                "Ética",
+                "Matemáticas",
+                "Inglés",
+                "Sociales"
+            ];
+
+            break;
+        case "8-1":
+            var materias = ["Biología",
+                "Agropecuaria",
+                "Español",
+                "Informatica",
+                "Tecnología",
+                "Artística",
+                "Educación Física",
+                "Ética",
+                "Matemáticas",
+                "Inglés",
+                "Sociales"
+            ];
+
+            break;
+        case "8-2":
+            var materias = ["Biología",
+                "Agropecuaria",
+                "Español",
+                "Informatica",
+                "Tecnología",
+                "Artística",
+                "Educación Física",
+                "Ética",
+                "Matemáticas",
+                "Inglés",
+                "Sociales"
+            ];
+
+            break;
+        case "9-1":
+            var materias = ["Biología",
+                "Agropecuaria",
+                "Español",
+                "Informatica",
+                "Tecnología",
+                "Artística",
+                "Educación Física",
+                "Ética",
+                "Matemáticas",
+                "Inglés",
+                "Sociales"
+            ];
+
+            break;
+        case "10-1":
+            var materias = ["Química",
+                "Agropecuaria",
+                "Español",
+                "Informatica",
+                "Ciencias Políticas",
+                "Artística",
+                "Educación Física",
+                "Ética",
+                "Matemáticas",
+                "Inglés",
+                "Sociales",
+                "Filosofía",
+                "Física"
+            ];
+
+            break;
+        case "11-1":
+            var materias = ["Química",
+                "Agropecuaria",
+                "Español",
+                "Informatica",
+                "Ciencias Políticas",
+                "Artística",
+                "Educación Física",
+                "Ética",
+                "Matemáticas",
+                "Inglés",
+                "Sociales",
+                "Filosofía",
+                "Física",
+                "emprendimiento"
+            ];
+
+            break;
+    };
+
+    for (option = 0; option < materias.length; option++) { //contador del array de materias
+        let option1 = document.createElement("option"); //creamos elemento option de html
+        option1.setAttribute("value", materias[option]); //añadimos el valor del elemento option
+        let option1Texto = document.createTextNode(materias[option]); //añadimos el valor del elemnto option visible al usuario
+        option1.appendChild(option1Texto); //añadimos el option a la lista 
+
+        materia.appendChild(option1); //añadimos el option al dropdown
+        document.body.appendChild(materia); //añadimos el dropdown al body del html 
+
+    };
+
+    function limpiar() { //funcion para limpiar el dropdown de materias
+        materia = document.getElementById('materia');
+        for (let i = materia.options.length; i >= 0; i--) {
+            materia.remove(i);
+        }
+    };
+
+}
+
+//funcion para validar el boton consultar
+function datos() {
+    alert("Base de datos ControlActividadesEscolares")
+}
+
+//funcion para validar losbotones de check
+function validarCheck() {
+    document.getElementById(flexRadioDefault1).innerHTML = "Entrando a revision de tareas"
+    alert("boton1")
+}
+
+/**
+ * fin bloque validación grados y materias perfil acudiente
+ */
